a8270374c13bae09e5717ac7ad910d4ce83850dd
Nicole <nicole.lee@berkeley.edu>
2025-07-30 05:25:00 -0700

    animated signature on intro card

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -1,12 +1,27 @@
 const IntroCard = () => {
   return (
-    <div className="relative max-w-[1000px]"> {/* Increase max width */}
-      <img src="intro-card-base.svg" alt="" className="w-full h-auto" /> {/* Make image take full width */}
-      <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="!font-poppins pl-24 pb-8 text-white text-8xl font-bold">Nicole Lee</h1>
+    <div className="relative max-w-[62.5rem] w-full">
+      {/* Background SVG */}
+      <img src="intro-card-base.svg" alt="" className="w-full h-auto" />
+
+      {/* Animated Signature Overlay */}
+      <div className="absolute inset-0">
+        <img
+          src="animated_signature.gif"
+          alt="Animated Signature"
+          className="
+            absolute
+            pointer-events-none
+            w-[90%]      // Resize with container
+            max-w-[60rem] // Prevent it from getting too big
+            left-[15%]   // Responsive horizontal position
+            top-[-12%]    // Responsive vertical position
+          "
+        />
       </div>
     </div>
   )
 }
+
 
 export default IntroCard
