22ba46f9977c37b66d6c36585a80d65c8e817dee
Nicole <nicole.lee@berkeley.edu>
2025-07-30 05:30:56 -0700

    resizable background component

---
+++ src/components/ResizableBackground.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ResizableBackground: React.FC = () => {
+  return (
+    <div className="background-container">
+      <img
+        src="/gallery/insurmate-1.png"
+        alt="Background"
+        className="background-image"
+      />
+    </div>
+  );
+};
+
+export default ResizableBackground;
