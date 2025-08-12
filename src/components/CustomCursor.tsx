ce286db2c25f6a712b6a2be70998961130c16be5
Nicole <nicole.lee@berkeley.edu>
2025-08-12 05:02:35 -0700

    cursor size

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -18,7 +18,7 @@ export default function CustomCursor() {
 
   return (
     <div
-      className="pointer-events-none fixed top-0 left-0 z-[9999] w-6 h-6 rounded-full transition-transform duration-75 ease-out"
+      className="pointer-events-none fixed top-0 left-0 z-[9999] w-5 h-5 rounded-full transition-transform duration-75 ease-out"
       style={{
         backgroundColor: "#1AC7C2", // light blue from your image
         transform: `translate(${position.x - 12}px, ${position.y - 12}px)`,
