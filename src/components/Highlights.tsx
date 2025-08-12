5dc0045d7d2a57c44ffba507ddc39164b4af7595
Nicole <nicole.lee@berkeley.edu>
2025-08-12 04:27:33 -0700

    font fix for highlights

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -3,7 +3,7 @@ import ScrollingGallery from "./ScrollingGallery"
 const Highlights = () => {
   return (
     <div className='items-center flex flex-col gap-7'>
-        <h1 className="!font-poppins text-5xl text-white font-bold">Here are some highlights!</h1>
+        <h1 className="!font-poppins text-5xl text-white font-extrabold">Here are some highlights!</h1>
         <button className="!font-poppins w-[210px] h-[40px] bg-[#F9C6CF] rounded-[29.5px] text-center justify-start text-[#bd1f20] font-semibold">See All Experiences →</button>
         <ScrollingGallery />
     </div>
