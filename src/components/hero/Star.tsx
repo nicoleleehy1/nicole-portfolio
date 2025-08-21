3d2b605a8c36a5b2360ed7590be77fc3bca1f986
Nicole <nicole.lee@berkeley.edu>
2025-08-21 18:22:10 -0400

    star component

---
+++ src/components/hero/Star.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function StarButton() {
+  return (
+    <div className="inline-block hover:animate-shimmer cursor-pointer">
+      <img src="star.svg" alt="" />
+    </div>
+  );
+}
